103dba05ffb0c3388fe6342043c8c0ef07bfd305
innicoder <innicoder@protonmail.com>
2022-06-10 01:06:29 +0200

    fix: removed unused code

--- news/main.py
+++ news/main.py
@@ -1,13 +1,9 @@
 import os
 
-from ctypes import sizeof
-
 from datetime import datetime
 from datetime import timedelta
 from deta import Deta
-from fastapi import Body
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from itertools import groupby
 from operator import itemgetter
@@ -40,10 +36,6 @@ class Item(BaseModel):
     date: str
 
 
-class Full(BaseModel):
-    items: list[Item]
-
-
 @app.get('/data/')
 def data():
     data = db.fetch().items
@@ -52,11 +44,10 @@ def data():
 
 @app.get('/data/today/top/')
 def data_today_top():
-    # fetch only from today
+
     yesterday = datetime.now() - timedelta(days=1)
     yesterday = yesterday.isoformat()
     data = db.fetch({'date?gte': f'{yesterday}'}).items
-
 
     content = []
     for k, v in groupby(data, itemgetter('domain')):
@@ -68,23 +59,14 @@ def data_today_top():
 
 @app.get('/data/month/top/')
 def data_month_top():
-    # fetch only from this month
+
     last_month = datetime.now() - timedelta(days=30)
     last_month = last_month.isoformat()
     data = db.fetch({'date?gte': f'{last_month}'}).items
- 
+
     content = []
     for k, v in groupby(data, itemgetter('domain')):
         sorted_data = sorted(v, key=itemgetter('views'), reverse=True)
         content.extend(sorted_data[:2])
 
     return content
-
-
-# @app.post('/feed/')
-# def post_feed(key: str, items: Full = Body(...)):
-#     if key == KEY:
-#         for item in items.items:
-#             db.put(jsonable_encoder(item), key=item.url)
-#         return {'status': 200}
-#     return {'status': 400}
